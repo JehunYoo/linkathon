f5872aee06483de8ae28282bbfd0c4a15e6d6aac
임종율 <dlawhdfbf12@naver.com>
2024-01-19 17:44:04 +0900

    :zap: Update:JWT 설정

--- Back/src/main/java/com/link/back/config/SecurityConfig.java
+++ Back/src/main/java/com/link/back/config/SecurityConfig.java
@@ -6,19 +6,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class AuthentificationConfig {
-
-
+public class SecurityConfig {
 
     // 인증이 필요없는 요청 URL
+    // 경력인증, 로그인, 회원가입
+    // 추가적으로 인증없이 사용할 수 있는 기능에 대해서 배열관리하기
     private static final String[] WHITE_LIST_URL = {
             "/api/users/**",
     };
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -31,14 +37,17 @@ public class AuthentificationConfig {
                 .sessionManagement((sessionManagement) ->
                                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                //로그인폼 사용 X -> 이건 질문할거임
+                //폼 로그인 사용 X -> 이건 질문할거임
                 .formLogin(AbstractHttpConfigurer::disable)
+                //베이직 접근차단
                 .httpBasic(AbstractHttpConfigurer::disable)
-                //토큰 감지 기능이 필요한데 좀 막힘 여기서
-                .addFilterBefore(new JwtFilter(setting), UsernamePasswordAuthenticationFilter.class)
+                //토큰 감지 기능이 필요한데 좀 막힘
+                //.addFilterBefore(new JwtFilter(setting), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
+                            //추가적으로
+                            .anyRequest().authenticated()
                 );
 
 
