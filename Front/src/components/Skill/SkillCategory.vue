3c971e91f27b92c383e35d2d388d0ca494bd2eb8
jmg97 <jmg97@kakao.com>
2024-01-22 17:40:29 +0900

    :bug: Fix:스킬카테고리 더미데이터 버그 수정

--- Front/src/components/Skill/SkillCategory.vue
+++ Front/src/components/Skill/SkillCategory.vue
@@ -1,34 +1,68 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import {Builder} from "builder-pattern";
+import SkillCategory from "@/components/Skill/SkillCategory.vue";
 
-const data: SkillDTO[][] = [];
-const skillCategoryList = ref<SkillDTO[][]>(data);
-const dummyList: skill[] = [];
-dummyList.push(Builder<SkillDTO>()
+const dummySkillList: SkillDTO[] = [];
+dummySkillList.push(Builder<SkillDTO>()
     .skillName("JavaScript")
     .skillId(1)
     .skillImgUrl("https://i.postimg.cc/C50Qnxmj/image.png")
     .build());
-dummyList.push(Builder<SkillDTO>()
+dummySkillList.push(Builder<SkillDTO>()
     .skillName("MjScript")
     .skillId(2)
     .skillImgUrl("https://i.postimg.cc/C50Qnxmj/image.png")
     .build());
-dummyList.push(Builder<SkillDTO>()
+dummySkillList.push(Builder<SkillDTO>()
     .skillName("TypeScript")
     .skillId(3)
     .skillImgUrl("https://i.postimg.cc/C50Qnxmj/image.png")
     .build());
 
-skillCategoryList.value.push(dummyList);
-skillCategoryList.value.push(dummyList);
-skillCategoryList.value.push(dummyList);
+const skillCategoryList: SkillCategoryDTO[] = [];
+const skillCategoryListRef = ref<SkillCategoryDTO[]>(skillCategoryList);
+
+skillCategoryList.push(
+    Builder<SkillCategoryDTO>()
+        .categoryName("언어")
+        .skillList(dummySkillList)
+        .build());
+
+skillCategoryList.push(
+    Builder<SkillCategoryDTO>()
+        .categoryName("프론트엔드")
+        .skillList(dummySkillList)
+        .build());
+
+skillCategoryList.push(
+    Builder<SkillCategoryDTO>()
+        .categoryName("백엔드")
+        .skillList(dummySkillList)
+        .build());
+
+skillCategoryList.push(
+    Builder<SkillCategoryDTO>()
+        .categoryName("테스팅 툴")
+        .skillList(dummySkillList)
+        .build());
+
+skillCategoryList.push(
+    Builder<SkillCategoryDTO>()
+        .categoryName("데이터베이스")
+        .skillList(dummySkillList)
+        .build());
+
+skillCategoryList.push(
+    Builder<SkillCategoryDTO>()
+        .categoryName("데이터")
+        .skillList(dummySkillList)
+        .build());
 </script>
 
 <template>
-  <div v-for="skillList in skillCategoryList">
-    {{skillList}}
+  <div v-for="skillList in skillCategoryListRef">
+    {{ skillList }}
   </div>
 </template>
 
