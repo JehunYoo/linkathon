09be2f65c52606a335702284244cd465d27a56a8
Jehun Yoo <percyu98@gmail.com>
2024-01-22 11:17:07 +0900

    :zap: Update: SkillType 한글화 작업

--- Back/src/main/java/com/link/back/entity/SkillType.java
+++ Back/src/main/java/com/link/back/entity/SkillType.java
@@ -1,13 +1,20 @@
 package com.link.back.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SkillType {
-	LANGUAGE,
-	FRONTEND,
-	BACKEND,
-	TESTING,
-	DATABASE,
-	DATA,
-	DEVOPS,
-	TOOL,
-	DESIGN
+	LANGUAGE("언어"),
+	FRONTEND("프론트엔드"),
+	BACKEND("백엔드"),
+	TESTING("테스팅 툴"),
+	DATABASE("데이터베이스"),
+	DATA("데이터"),
+	DEVOPS("데브옵스"),
+	TOOL("협업툴"),
+	DESIGN("디자인");
+
+	private final String name;
 }
