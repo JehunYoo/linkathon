b66ad5ea8c197125475f71f960efa4e9e5e8a218
Jehun Yoo <percyu98@gmail.com>
2024-01-22 10:03:40 +0900

    :zap: Update: 사용하지 않는 import 제거

--- Back/src/main/java/com/link/back/entity/UserImage.java
+++ Back/src/main/java/com/link/back/entity/UserImage.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
