56544ad1a2544f29b47721b02de204625143e8e5
jmg97 <jmg97@kakao.com>
2024-01-23 15:39:02 +0900

    :sparkles: Feat: 블러 컴포넌트 추가

---
+++ Front/src/components/Blur.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="blur"/>
+</template>
+
+<style scoped>
+.blur {
+  z-index: 3;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(255, 255, 255, 0.70);
+  backdrop-filter: blur(5px);
+  transform: translate(-50%, -50%)
+}
+</style>
