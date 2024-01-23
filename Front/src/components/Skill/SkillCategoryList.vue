6c7130e24ac9a298e19ed251b38bbbd9933d5ba1
jmg97 <jmg97@kakao.com>
2024-01-23 13:36:31 +0900

    :sparkles: Feat:기술 카테고리 리스트 추가

---
+++ Front/src/components/Skill/SkillCategoryList.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {inject} from "vue";
+import SkillIcon from "@/components/Skill/SkillIcon.vue";
+
+const skillCategorySelect: SkillCategorySelect = inject('skillCategorySelect')!;
+const updateSelectSkillId: Function = inject<Function>('updateSelectSkillId')!;
+</script>
+
+<template>
+  <div v-if="skillCategorySelect.skillCategoryList[skillCategorySelect.selectedCategory]" class="skill-list-container">
+    <div v-for="skillDTO in skillCategorySelect.skillCategoryList[skillCategorySelect.selectedCategory].skillList"
+         class="skill-button-container"
+         @click="updateSelectSkillId(skillDTO.skillId)">
+      <SkillIcon :skill="skillDTO" width="40px" height="40px" radius="10px"/>
+      <div class="skill-button-text">{{ skillDTO.skillName }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+
+.skill-list-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  margin-top: 16px;
+}
+
+.skill-button-container {
+  display: flex;
+  min-width: 148px;
+  gap: 16px;
+}
+
+.skill-button-text {
+  color: #303030;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 40px;
+}
+</style>
