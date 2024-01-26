6e69b46d7fd3db656114a1c77857424ba484b186
MaSeongJin <making1104@naver.com>
2024-01-26 15:56:26 +0900

    :sparkles: Feat: Config 내용 추가

--- Back/src/main/java/com/link/back/config/QuerydslConfig.java
+++ Back/src/main/java/com/link/back/config/QuerydslConfig.java
@@ -1,2 +1,21 @@
-package com.link.back.config;public class QuerydslConfig {
+package com.link.back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfig {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
 }
