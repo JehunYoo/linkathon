01b09faff3d075d6037015e4bd7cfa53d9135f4a
김지민 <mnvyiions@gmail.com>
2024-01-29 10:11:47 +0900

    :bug: Fix: UserImage Entity의 User 연결 삭제

--- Back/src/main/java/com/link/back/entity/UserImage.java
+++ Back/src/main/java/com/link/back/entity/UserImage.java
@@ -1,7 +1,6 @@
 package com.link.back.entity;
 
 import static com.link.back.config.AppConstant.*;
-import static jakarta.persistence.FetchType.*;
 import static jakarta.persistence.GenerationType.*;
 import static lombok.AccessLevel.*;
 
@@ -9,7 +8,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,9 +19,6 @@ public class UserImage {
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
 	private Long userImageId;
-
-	@OneToOne(fetch = LAZY)
-	private User user;
 
 	@Column(length = USER_IMAGE_NAME_LENGTH, nullable = false)
 	private String userImageName;
