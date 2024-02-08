1ddbe11bdff6564619b5fce72a07a4650767309b
김지민 <mnvyiions@gmail.com>
2024-02-08 09:30:33 +0900

    :sparkles: Feat: 오픈비두 컴포넌트 생성

---
+++ Front/src/components/OpenVidu/OvVideo.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import {onMounted} from "vue";
+
+const props = defineProps({
+  streamManager: Object,
+});
+
+onMounted(() => {
+  props.streamManager.addVideo
+});
+
+</script>
+
+<template>
+  <video autoplay/>
+</template>
+
+<style scoped></style>
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: 'OvVideo',-->
+
+<!--  props: {-->
+<!--    streamManager: Object,-->
+<!--  },-->
+
+<!--  mounted () {-->
+<!--    this.streamManager.addVideoElement(this.$el);-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
