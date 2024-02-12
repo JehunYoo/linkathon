d9a0eafcd966dbefeb993c7629accfbb0d70a788
jmg97 <jmg97@kakao.com>
2024-02-12 17:48:03 +0900

    :zap: Update: 해커톤 리워드 버그 수정

--- Front/src/components/Hackathon/HackathonRewardTeam.vue
+++ Front/src/components/Hackathon/HackathonRewardTeam.vue
@@ -3,15 +3,16 @@
 import SkillIcon from "@/components/Skill/SkillIcon.vue";
 import {PropType, watchEffect} from "vue";
 import {WinnerProjectResponseDto} from "@/dto/tmpDTOs/hackathonDTO.ts";
+import {Builder} from "builder-pattern";
 
 const props = defineProps({
-  data : {
-    type : Object as PropType<WinnerProjectResponseDto>,
+  data: {
+    type: Object as PropType<WinnerProjectResponseDto>,
     required: true
   },
-  id : {
-    type : Number,
-    required : true
+  id: {
+    type: Number,
+    required: true
   }
 });
 watchEffect(() => {
@@ -26,19 +27,19 @@ watchEffect(() => {
     <img :src="props.data?.winnerProjectInfoDto?.imgSrc" alt=""/>
     <h1>프로젝트 설명</h1>
     <section>
-      {{props.data.winnerProjectInfoDto?.projectDesc}}
+      {{ props.data.winnerProjectInfoDto?.projectDesc }}
     </section>
     <h1>팀원 목록</h1>
     <div class="member-img-container">
       <img :src=member.userImageUrl alt=""
            class="member-img" v-for="member in props.data.teamResponseDto?.members">
-
     </div>
     <h1>기술 스택</h1>
     <div class="skill-container">
       <div v-for="skill in props.data?.teamResponseDto?.teamSkills">
-        <img :src="skill.skillImageUrl" alt="" width="52px" height="52px" radius="10px"/>
-<!--        <SkillIcon :skill="skill" width="52px" height="52px" radius="10px"/>-->
+        <SkillIcon :skill="Builder<SkillDTO>().skillName(skill.skillName).skillImgUrl(skill.skillImageUrl).build()"
+                   width="52px"
+                   height="52px" radius="10px"/>
       </div>
     </div>
   </div>
