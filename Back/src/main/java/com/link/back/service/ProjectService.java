fa6a37f06c91e43c2b312cba1e0bbaf3d75349b9
MaSeongJin <making1104@naver.com>
2024-02-14 01:48:15 +0900

    :zap: Update: 레드마인 관련 api 수정

--- Back/src/main/java/com/link/back/service/ProjectService.java
+++ Back/src/main/java/com/link/back/service/ProjectService.java
@@ -305,4 +305,12 @@ public class ProjectService {
 		}
 		return false;
 	}
+
+	public boolean isMyProject(Long projectId, Long userId) {
+		return projectRepository.findById(projectId)
+			.map(project -> project.getTeam().getUserTeamList().stream()
+				.anyMatch(userTeam -> userTeam.getUser().getUserId().equals(userId)
+					&& userTeam.getMemberStatus() == JOINED))
+			.orElse(false);
+	}
 }
