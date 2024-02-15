1c8a660176c756d9fe54a5e8b890e304fad93f38
jmg97 <jmg97@kakao.com>
2024-02-15 14:12:19 +0900

    :zap: Update:슛!

--- Back/src/main/java/com/link/back/controller/Test.java
+++ Back/src/main/java/com/link/back/controller/Test.java
@@ -3,11 +3,13 @@ package com.link.back.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/api/test")
 public class Test {
-	@GetMapping("/")
+	@GetMapping("/test")
 	ResponseEntity<?> test() {
-		return ResponseEntity.ok().build();
+		return ResponseEntity.ok("HI");
 	}
 }
