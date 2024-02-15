07f682cb2541cf3ae918adf82bbd3116f1951ebe
jmg97 <jmg97@kakao.com>
2024-02-15 15:39:26 +0900

    :zap: Update:슛!

--- Back/src/main/java/com/link/back/controller/Test.java
+++ Back/src/main/java/com/link/back/controller/Test.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
 	@GetMapping("/test")
 	ResponseEntity<?> test() {
-		return ResponseEntity.ok("AMAZONE");
+		return ResponseEntity.ok("PONG PONG PONG");
 	}
 }
