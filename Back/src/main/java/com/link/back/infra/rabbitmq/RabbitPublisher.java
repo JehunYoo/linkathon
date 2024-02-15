2bb8f0a712a06423d605b64f602448c209f87a83
주윤 <joo6881@naver.com>
2024-02-15 16:20:38 +0900

    배치 테ã스트

--- Back/src/main/java/com/link/back/infra/rabbitmq/RabbitPublisher.java
+++ Back/src/main/java/com/link/back/infra/rabbitmq/RabbitPublisher.java
@@ -25,7 +25,7 @@ public class RabbitPublisher {
 		rabbitTemplate.convertAndSend("sonarqube_queue", combinedMessage);
 	}
 
-	@Scheduled(cron = "0 47 15 * * *", zone = "Asia/Seoul")  // 매일 자정에 실행
+	@Scheduled(cron = "0 24 16 * * *", zone = "Asia/Seoul")  // 매일 자정에 실행
 	public void sendMessagesDaily() {
 		Iterable<Project> projects = projectRepository.findAll();
 		for (Project project : projects) {
