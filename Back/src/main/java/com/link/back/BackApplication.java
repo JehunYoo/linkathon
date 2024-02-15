c20b59dbbef7afe25d6fa87a39f141e62e087348
주윤 <joo6881@naver.com>
2024-02-15 12:38:11 +0900

    배치 테ã스트

--- Back/src/main/java/com/link/back/BackApplication.java
+++ Back/src/main/java/com/link/back/BackApplication.java
@@ -1,5 +1,7 @@
 package com.link.back;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,10 +9,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
 @EnableScheduling
+
 @SpringBootApplication
 public class BackApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));	// EC2에서도 Tomcat 서버의 시간을 서울 시간으로 변경한다.
 		SpringApplication.run(BackApplication.class, args);
 	}
 
--- Back/src/main/java/com/link/back/infra/rabbitmq/RabbitPublisher.java
+++ Back/src/main/java/com/link/back/infra/rabbitmq/RabbitPublisher.java
@@ -25,7 +25,7 @@ public class RabbitPublisher {
 		rabbitTemplate.convertAndSend("sonarqube_queue", combinedMessage);
 	}
 
-	@Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")  // 매일 자정에 실행
+	@Scheduled(cron = "0 40 12 * * *", zone = "Asia/Seoul")  // 매일 자정에 실행
 	public void sendMessagesDaily() {
 		Iterable<Project> projects = projectRepository.findAll();
 		for (Project project : projects) {
