be781b6f8b229f2e1d1b8884b21f9cfd246403fe
MaSeongJin <making1104@naver.com>
2024-02-15 14:20:51 +0900

    :bug: Fix: 팀 권유받은 팀 수락 신청 버튼 버그 수정

--- Front/src/components/MyPage/MyPageRecruitTeamInfo1.vue
+++ Front/src/components/MyPage/MyPageRecruitTeamInfo1.vue
@@ -14,13 +14,13 @@ const router =  useRouter();
 const team = ref<number>(0);
 const refTeamId = ref<number>(0);
 
-const acceptSuggestion = async () => {
-  await teamService.postSuggestionByUser(refTeamId.value);
+const acceptSuggestion = (teamId : number) => {
+  teamService.postSuggestionByUser(teamId.value);
   location.href = "/myPage";
 }
 
-const declineSuggestion = async () => {
-  await teamService.deleteSuggestionByUser(refTeamId.value);
+const declineSuggestion = (teamId : number) => {
+  teamService.deleteSuggestionByUser(teamId.value);
   location.href = "/myPage";
 }
 
@@ -59,8 +59,8 @@ onMounted(async () => {
   <hr>
   <div class="title-container">
     <h1 class="btn">{{ refTeam?.teamName }}</h1>
-    <div class="accept-button" @click="acceptSuggestion">수락</div>
-    <div class="remove-button" @click="declineSuggestion">거절</div>
+    <div class="accept-button" @click="acceptSuggestion(refTeam?.teamId)">수락</div>
+    <div class="remove-button" @click="declineSuggestion(refTeam?.teamId)">거절</div>
   </div>
   <h1 class="teamDesc">{{ refTeam?.teamDesc }}</h1>
   <h1>해커톤 정보</h1>
